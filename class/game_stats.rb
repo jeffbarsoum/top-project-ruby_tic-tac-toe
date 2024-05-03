31a0f67741deeadc4ec3385628b0fae4223db082
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-05-03 15:21:16 -0400

    Created GameStats class

---
+++ class/game_stats.rb
@@ -0,0 +1,30 @@
+class GameStats
+  @stats = {
+    score: {x: 0, o: 0}
+    turn: {x: 0, o: 0}
+    round: 0
+    }
+
+  def initialize
+    @stats[:score] = {x: 0, o: 0}
+    @stats[:turn] = {x: 0, o: 0}
+    @stats[:round] = 0
+  end
+
+  def score
+    @stats[:score]
+  end
+
+  def turn
+    @stats[:turn]
+  end
+
+  def round
+    @stats[:round]
+  end
+
+  def add_stat stat, player = nil
+    return @stats[stat.to_sym] += 1 if stat.to_sym == :round
+    @stats[stat.to_sym][player.to_sym] += 1
+  end
+end
