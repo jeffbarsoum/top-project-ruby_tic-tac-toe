35d20682cda95f65c4257b9e33276e50c93d9301
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-05-04 03:24:40 -0400

    Defined getter for Squares in GameData module

--- module/game_data.rb
+++ module/game_data.rb
@@ -1,14 +1,13 @@
+require_relative "game_error"
+
 module GameData
   attr_reader :squares
+
+  include GameError
 
 
   def initialize
     @squares = {
-      nil: [
-        ['', '', '', '', ''],
-        ['', :coord_col '', :coord_row ''],
-        ['', '', '', '', '']
-      ]
       x: {
         place: [
           ['x', '', '', '', 'x'],
@@ -65,4 +64,46 @@ module GameData
       }
     }
   end
+
+  # if player is nil, piece should be a string of coordinates
+  # otherwise, it's a symbol representing the placement type
+  def squares piece, player = nil
+    cls_name = "GameData"
+    func_name = "squares"
+    begin
+      return self.nil_square piece unless player
+
+      raise GamePieceError unless self.squares.key? piece
+      self.squares[piece]
+    rescue GamePieceError
+      msg_err = "piece must exist in squares hash!"
+      puts GameError.game_error cls_name, func_name, msg_err
+    end
+  end
+
+  def nil_square coordinates
+    cls_name = "GameData"
+    func_name = "nil_square"
+    begin
+      unless player
+        row_coord = coordinates[0]
+        col_coord = coordinates[1]
+
+        is_valid_col = ('a'..'z').to_a.include? col_coord
+        is_valid_row = (1..9).to_a.include? row_coord
+
+        raise GamePieceError unless is_valid_col && is_valid_row
+
+        [
+          ['', '', '', '', ''],
+          ['', col_coord '', row_coord ''],
+          ['', '', '', '', '']
+        ]
+      end
+    rescue GamePieceError
+      msg_err = "coordinates must be an alphabetic character followed by an integer!"
+      puts GameError.game_error cls_name, func_name, msg_err
+    end
+  end
+
 end
