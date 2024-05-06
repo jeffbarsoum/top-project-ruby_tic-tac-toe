5b198a52a978646700bde3f1506395a0aee89a70
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-05-05 23:30:19 -0400

    Created Save Module wireframe

---
+++ module/save.rb
@@ -0,0 +1,26 @@
+require 'yaml'
+
+module Save
+
+  @@data_list = []
+
+  attr_reader :save_directory, :data_list
+
+  def initialize save_directory
+
+  end
+
+  def save_file data, file_name
+
+  end
+
+  def load_file file_name
+
+  end
+
+  def get_save_data_list
+
+  end
+
+
+end
