734aee6ce267cb8a2d1a76a16be54aae8b953816
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-05-16 20:11:58 -0400

    Added user options

--- class/tic_tac_toe.rb
+++ class/tic_tac_toe.rb
@@ -1,20 +1,38 @@
 require_relative "module/get_user_input"
+require_relative "module/game_error"
 require_relative "game_board"
 require_relative "player"
 require_relative "game_stats"
 require_relative "game_display"
+require_relative "game_save"
 
 class TicTacToe
   SQUARE_CHOICES = [:x, :o, :nil]
+  USER_OPTIONS = {
+    start: "o",
+    quit: "q",
+    save: "s",
+    load: "l"
+  }
 
   include GetUserInput
+  include GameError
 
-  attr_reader :game_board, :players, :stats, :display
+  attr_reader :game_board, :players, :stats, :display, :save
 
+  def process_user_option input
+    return unless USER_OPTIONS.has_val? input
+    command = USER_OPTIONS.key input
+    command || false
+  end
 
-  def game_error class_name, function_name, error_message
-    "#{class_name}.#{function_name}  ERROR: #{error_message}"
-  return
+  def get_current_player
+    self.players[0]
+  end
+
+  def get_next_player
+    self.players[1]
+  end
 
   def return_user_input message, multi_entry, user_options
     GetUserInput.return_user_input message, multi_entry, user_options
@@ -26,7 +44,8 @@ class TicTacToe
 
     @players = []
     until Player.get_free_players.empty? do
-      name = self.return_user_input message, false
+      msg = "What would you like us to call you this round?"
+      name = self.return_user_input msg, false
       self.players.push Player.new name
     end
 
@@ -52,9 +71,8 @@ class TicTacToe
   def play_game
     is_quit = nil
     until is_quit do
-      is_winner = false
-      until is_winner do
-        is_winner = self.play_turn
+      until @stats.winner do
+        @stats.winner = self.play_turn
       end
       self.game_board.game_matrix.assign_winner is_winner
       msg = "#{current_player.name}, your turn!"
