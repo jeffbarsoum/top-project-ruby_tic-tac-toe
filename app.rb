b1ceaca14dcd600f144ca19e718f5898114bfd3b
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-05-30 08:04:07 -0400

    Add load paths to boot script

--- app.rb
+++ app.rb
@@ -1,3 +1,11 @@
-require_relative "class/tic_tac_toe"
+# Add load paths here:
+$LOAD_PATH.unshift('save')
+$LOAD_PATH.unshift('module')
+$LOAD_PATH.unshift('class')
+$LOAD_PATH.unshift('state')
 
+# load required scripts here:
+require "tic_tac_toe"
+
+# launch application:
 TicTacToe.new
