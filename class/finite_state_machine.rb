91d5382163dabfdefadf26e76f2881a18a2698ed
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-05-30 08:08:07 -0400

    Build load_state method to output a class instance

--- class/finite_state_machine.rb
+++ class/finite_state_machine.rb
@@ -2,7 +2,7 @@ require_relative "tic_tac_toe"
 
 class FiniteStateMachine < TicTacToe
 
-  attr_reader :state_dir, :state_files, :states, :classes
+  attr_reader :state_dir, :state_files, :states, :classes, :instances
 
 
   def error class_name, function_name, error_message
@@ -15,16 +15,28 @@ class FiniteStateMachine < TicTacToe
     @state_files = self.get_state_file_list
     @states = []
     @classes = []
+    @instances = []
   end
 
-  def load_state state
-    load_str = "../#{self.state_dir}/#{state}.rb"
-    require_relative "#{load_str}/#{state}.rb"
-    return state_files
+  def load_state state_file, args, state_commands, screen_commands
+    return false unless self.state_files.include? state_file
+    require state
+
+    state_class_name =  self.parse_state_file state_file
+    state_class = Object.const_get state_class_name
+
+    if state_class
+      self.classes.unshift { state_class_name => state_class }
+
+      state_class_instance = state_class.new args, state_commands, screen_commands
+      self.instances.unshift { state_class_name => state_class_instance }
+    end
+
+    return state_class_instance
   end
 
-  def unload_state
-    Object.send(:remove_const, self.classes.unshift)
+  def unload_state class_name
+
   end
 
   def parse_state_file state_file
@@ -32,7 +44,7 @@ class FiniteStateMachine < TicTacToe
     func_name = "parse_state_file"
     begin
       raise FiniteStateMachineError unless self.state_files.include? state_file
-      state_file_processed = state_file.replace ".rb", "" .split "_" .map! { |word| word.capitalize } .join
+      state_file_processed = state_file.replace ".rb", "" .split "_" .map! { |word| word.capitalize } .join "" .to_sym
     rescue FiniteStateMachineError
       msg_fsm_error <<-STRING
       #{state_file} not found in #{self.state_dir} directory!
