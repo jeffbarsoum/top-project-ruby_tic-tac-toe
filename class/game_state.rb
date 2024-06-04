f9ee511296277502f195748a1b3e6760c06f171e
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-04 10:00:58 -0400

    Move default state opts to Data class

--- class/game_state.rb
+++ class/game_state.rb
@@ -9,15 +9,9 @@ class GameState
   include Data
   include Display
 
-  attr_reader :cmd
+  attr_reader :state_name, :state_opts, :cmd
 
   @@fsm = FiniteStateMachine.new self.cmd_hash, "game_state"
-
-  @opts = {
-    state: :title
-    cmds: [:start, :load, :save, :quit],
-    vertical: true
-  }
 
 
   def self.load_next_state opts
@@ -28,40 +22,38 @@ class GameState
     @@fsm.load_state offset
   end
 
-  def self.opts_out **params
-    opts_hash = {
-      state_file: params[:state_file] || nil,
-      args: params[:args] || [],
-      state_cmds: params[:state_cmds] || [],
-      screen_cmds: params[:screen_cmds] || [],
-      vertical: params[:vertical] || true
-    }
 
-    params.reduce opts_hash do | opts, (param, value) |
-      opts[param] = value unless opts[param]
-      opts
+  def initialize **opts
+    @state_opts = self.generate_opts self.state_name, opts
+    user_input = self.screen opts
+    @cmd = self.cmd user_input
+    self.create_cmd
+    self.cmd
+  end
+
+  def state_name
+    self.class.name.reduce "" do | name, letter |
+      name += letter if letter == letter.downcase
+      name += "_#{letter.downcase}" if letter == letter.upcase
+      name
     end
   end
 
-
-  def initialize **opts
-    @opts = opts
-    user_input = self.screen opts
-    @cmd = self.cmd user_input
-  end
-
-  def opts param = nil
+  def state_opts param = nil
     return @opts[param.to_sym] if param
     @opts
   end
 
-  def opts=param, value
+  def state_opts=param, value
     @opts[param.to_sym] = value
   end
 
-  def run_cmd opts
+  def create_cmd
     return self.send self.cmd.to_s if self.respond_to? self.cmd
     self.define_method self.cmd.to_s do { | arg | self.class.load_next_state arg }
+  end
+
+  def run_cmd opts
     self.send self.cmd, opts
   end
 
--- module/data.rb
+++ module/data.rb
@@ -3,7 +3,7 @@ require "error"
 module Data
   include Error
 
-  attr_reader :cmds, :squares
+  attr_reader :cmds, :opts, :squares
 
 
   def initialize
@@ -15,6 +15,13 @@ module Data
       b: :back,
       r: :reset,
       a: :play_again
+    }
+
+    @opts = {
+      title: { state_file:"title", state_cmds: [:start, :load, :save, :quit], vertical: true },
+      load: { state_file:"load", state_cmds: [:back, :quit], vertical: false },
+      game: { state_file:"game", state_cmds: [:save, :quit], vertical: true },
+      win: { state_file:"win", state_cmds: [:play_again, :save, :quit], vertical: true },
     }
 
     @squares = {
@@ -130,5 +137,19 @@ module Data
     end
   end
 
+  def generate_opts state, **params
+    opts_default = self.opts[state.to_sym]
+    opts_hash = {
+      state_file: opts_default[:state_file] || params[:state_file] || nil,
+      args: opts_default[:args] || params[:args] || [],
+      state_cmds: opts_default[:state_cmds] || params[:state_cmds] | [],
+      screen_cmds: opts_default[:screen_cmds] || params[:screen_cmds] || [],
+      vertical: opts_default[:vertical] || params[:vertical] || true
+    }
+    params.reduce opts_hash do | opts, (param, value) |
+      opts[param] = value unless opts[param]
+      opts
+    end
+  end
 
 end
