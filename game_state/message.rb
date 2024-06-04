7131a33846e19d03e7d8bd30e495b0025e42258c
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-04 15:25:16 -0400

    Message state wireframe

---
+++ game_state/message.rb
@@ -0,0 +1,52 @@
+require "game_state"
+
+class Title < GameState
+
+  attr_reader :cmd
+
+
+  def state_opts param = nil
+    super param
+  end
+
+  def state_opts=param, value
+    super param, value
+  end
+
+  def run_cmd opts
+    super opts
+  end
+
+
+  def initialize **opts
+    super opts
+  end
+
+  def display _args = {}
+    msg_screen <<-STRING
+    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
+                                        Tic Tac Toe!
+    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
+
+    Please press a key to choose your option:
+
+    STRING
+  end
+
+  def start
+
+  end
+
+  def load
+
+  end
+
+  def save
+
+  end
+
+  def quit
+
+  end
+
+end
