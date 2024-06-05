1d7fd9913f7f4a8b1a5196befed85cd18e485c8e
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-05 09:07:23 -0400

    DataBoard module wireframe

---
+++ data/board.rb
@@ -0,0 +1,24 @@
+module DataBoard
+
+  def square_size
+    [3, 5]
+  end
+
+  def default_board_size
+    3
+  end
+
+  def max_board_size
+    7
+  end
+
+  def border
+    {
+      top: "_",
+      side: "|",
+      bottom: "-",
+      corner: "+"
+    }
+  end
+
+end
