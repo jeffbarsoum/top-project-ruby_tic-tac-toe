67682a5c9bb4192034ecf1667a7daca486fa57ce
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-05 23:53:07 -0400

    Add class variable for player instances, file reorg

--- class/player.rb
+++ class/player.rb
@@ -1,25 +1,13 @@
 require "error"
 
 class Player
+
   @@free_players = [:x, :o]
   @@chosen_players = []
+  @@players = []
+
   attr_reader :name, :player, :score
 
-  def initialize name
-    begin
-    rescue NoFreePlayerError
-      puts "ERROR: All players selected!"
-    else
-      raise NoFreePlayerError if @@free_players.empty?
-
-      @name = name
-      @player = self.class.choose_player
-      @score = 0
-
-
-      puts "Hello, #{self.name}, you will be '#{self.player}''s"
-    end
-  end
 
   def self.choose_player
     player_index = @@free_players.length > 1 ? Random.rand().round : 0
@@ -28,12 +16,35 @@ class Player
     choose_player
   end
 
-  def self.get_free_players
+  def self.free_players
     @@free_players
   end
 
-  def self.get_chosen_players
+  def self.chosen_players
     @@chosen_players
+  end
+
+  def self.player_join=p
+    @@players.push p
+  end
+
+  def get_players index = nil
+    return @@players[index] if index
+    @@players
+  end
+
+
+  def initialize name
+    begin
+      raise NoFreePlayerError if @@free_players.empty?
+
+      @name = name
+      @player = self.class.choose_player
+      @score = 0
+      self.class.player_join = self
+    rescue NoFreePlayerError
+      self.error "All players selected!"
+    end
   end
 
   def add_score
