54deaa1cedf17789adb2f47eb2a26fbb17c9143e
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-08 04:23:01 -0400

    Remove FSM references and Variablize methods

--- class/game_state.rb
+++ class/game_state.rb
@@ -9,23 +9,11 @@ class GameState
   include Display
   include Variablize
 
-  attr_reader :state_cmd, :screen_cmd
+  attr_reader :state_cmds, :state_opts
 
-  @@fsm = FiniteStateMachine.new self.cmd_hash, "game_state"
   @@data = Data.new dir: "data", class_prefix: "Data"
 
-
-  def self.load_next_state opts
-    @@fsm.load_next_state opts
-  end
-
-  def self.load_state offset = 0
-    @@fsm.load_state offset
-  end
-
-
   def initialize **opts
-    @state_opts = self.data.generate_opts self.state_name, opts
     user_input = self.screen opts
     user_output = self.data.cmd user_input
 
@@ -39,17 +27,6 @@ class GameState
 
   end
 
-  def state_name
-    self.class.name.reduce "" do | name, letter |
-      name += letter if letter == letter.downcase
-      name += "_#{letter.downcase}" if letter == letter.upcase
-      name
-    end
-  end
-
-  def fsm
-    @@fsm
-  end
 
   def data
     @@data
@@ -57,7 +34,7 @@ class GameState
 
   def state_opts param = nil
     return @state_opts[param.to_sym] if param
-    @opts
+    @state_opts
   end
 
   def state_opts=param, value
