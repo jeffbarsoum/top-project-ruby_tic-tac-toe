857f9449cc88c0922aad9d71638c757f302ff7c1
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-08 09:36:57 -0400

    Simplify cmds (Still may deprecate)

--- data/cmds.rb
+++ data/cmds.rb
@@ -14,47 +14,14 @@ module DataCmds
 
   def cmds
     {
-      start: {
-        user_input: "o",
-        state_file: "title",
-        state_cmds: [:start, :load, :save, :quit],
-        proc_opts: Proc.new { self.generate_opts "title" },
-        proc_load: Proc.new { self.fsm.load_next_state "title", args, opts }
-      },
-      quit: {
-        user_input: "q",
-        state_file: "quit",
-        state_cmds: [:yes, :no],
-        proc_opts: Proc.new { self.generate_opts "quit" },
-        proc_load: Proc.new { self.fsm.load_next_state "quit", args, opts }
-      },
-      save: {
-        user_input: "s",
-        state_file: "save",
-        state_cmds: [:back, :quit],
-        proc_opts: Proc.new { self.generate_opts "save" },
-        proc_load: Proc.new { self.fsm.load_next_state "save", args, opts }
-      },
-      load: {
-        user_input: "l",
-        state_file: "load",
-        state_cmds: [:back, :quit],
-        proc_opts: Proc.new { self.generate_opts "load" },
-        proc_load: Proc.new { self.fsm.load_next_state "load", args, opts }
-      },
-      back: {
-        user_input: "b",
-        proc_load: Proc.new {
-          back_state = self.fsm.load_state 1
-        }
-      },
-      reset: {
-        user_input: "r",
-        state_file: "title",
-        state_cmds: [:start, :load, :save, :quit],
-        proc_opts: Proc.new { self.generate_opts "title" },
-        proc_load: Proc.new { self.fsm.load_next_state "title", args, opts }
-      },
+      input: {
+        "input"
+      load: :l,
+      start: :o,
+      quit: :q,
+      save: :s,
+      back: :b,
+      reset: :reset,
       a: :play_again
     }
   end
