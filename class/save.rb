8c916ed7173fb3abbf4b1cfc4fecf93feb00bc14
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-10 06:28:22 -0400

    Change save to it's own class  (will deprecate GameState)

--- class/save.rb
+++ class/save.rb
@@ -1,18 +1,23 @@
 require 'yaml'
 
-module Save
+class Save
 
-  attr_reader :save_directory, :data_list
+  attr_reader :save_directory, :data_list, :load_data
 
-  def initialize save_directory = "save_data"
+  def initialize save_directory = "save"
     # making path relative to app's parent directory
     @save_directory = "../#{save_directory}"
     @data_list = self.get_save_data_list self.save_directory
+    @load_data = {}
+
+    self.data_list.each do |data_file|
+      @load_data[data_file.to_sym] = self.load_file data_file
+    end
 
   end
 
   def save_file data, key, overwrite? = true
-    can_write_file = !self.is_data_exists || overwrite
+    can_write_file = !self.is_data_exists? || overwrite
     return false unless can_write_file
     File.open "#{self.save_directory}#{key}.yml", "w" { |file| file.write data.to_yaml }
     push self.data_list "#{key}.yml" unless self.is_data_exists
