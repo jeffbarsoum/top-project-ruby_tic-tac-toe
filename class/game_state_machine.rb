8fe0989b4aaccb209718fcffa511cfbaea6503a5
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-11 15:27:00 -0400

    GameStateMachine Wireframe

--- class/game_state_machine.rb
+++ class/game_state_machine.rb
@@ -2,57 +2,90 @@ require "error"
 require "variablize"
 
 class GameStateMachine < StateMachine
-  include Error
-  include Variablize
+  attr_reader :state_dir
 
-  attr_reader :state_dir, :state_files, :states, :game_save
-
-
-  def initialize state_dir = "state"
+  # Parent Methods: All finite state machines will use these
+  def initialize state_dir = "game_state"
     @state_dir = state_dir
-    @state_files = self.get_state_file_list
-    @states = []
-    @game_save = {}
+   super state_dir
   end
 
   def load_state state_file:, **opts
-    return false unless self.state_files.include? state_file
-    require state_file
-
-    cls_name =  self.class_name state_file
-    cls = Object.const_get cls_name
-
-    if cls
-      cls_instance = cls.new opts
-      @states.unshift { cls_name => cls_instance }
-      @states.pop if @states.length > 10
-    end
-
-    cls_instance
+    super state_file, opts
   end
 
   def get_state offset = 0, cls_skip = []
-    until offset > self.states.length - 1 do
-      back_state = self.states[offset]
-      is_skip = cls_skip.include? back_state.class.name
-      return back_state unless is_skip
-      offset += 1
-    end
+    super offset, cls_skip
   end
 
   def get_state_file_list
-    Dir "../#{self.state_dir}/" .map do |state_file|
-      state_file.basename
-    end
+    super
   end
 
   def game_save
-    @game_save[:state_dir] = self.state_dir
-    @game_save[:state_files] = self.state_files
-    self.instances.each do |state|
-      @game_save[:states].push state.game_save
-    end
-    self.game_save
+    super
   end
 
+
+  # State Methods: Each method launches a game_state instance
+  def back **opts
+    self.get_state 1, ["Message", "Input"]
+  end
+
+  def input **opts
+    self.load_state state_file: __method__.to_s
+  end
+
+  def load **opts
+    self.load_state state_file: __method__.to_s
+  end
+
+  def message **opts
+    self.load_state state_file: __method__.to_s
+  end
+
+  # NOTE: player functions should be placed elsewhere
+  # (another class, perhaps)
+  def play matrix:, players:, stats:, **opts
+    coordinates = matrix.coordinates
+    until stats.winner || coordinates.empty? do
+      stats.add_turn self.get_current_player
+      opts = {
+        matrix: matrix,
+        players: players,
+        stats: stats
+      }
+      next_state = self.load_state state_file: __method__.to_s, opts
+      cmds = next_state.state_opts "state_cmds"
+      cmd = next_state.get_next_state
+      return cmd if cmds.keys.include? cmd
+      stats.add_winner "winner", matrix.assign_piece self.get_current_player, cmd
+      self.change_player_turn
+    end
+    stats.add_stat "round"
+    if stats.winner
+      winner = stats.winner[:player]
+      stats.add_score winner
+      return "win"
+    else
+      stats.add_score "draw"
+      return "draw"
+    end
+  end
+
+  def save **opts
+    self.load_state state_file: __method__.to_s
+  end
+
+  def title **opts
+    self.load_state state_file: __method__.to_s
+  end
+
+  def quit **opts
+    self.load_state state_file: __method__.to_s
+  end
+
+  def win **opts
+    self.load_state state_file: __method__.to_s
+  end
 end
