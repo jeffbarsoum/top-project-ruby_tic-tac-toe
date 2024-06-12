ce6a6e6847862bfb631170f028fcb3c23acd8b0b
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-12 07:01:35 -0400

    Moved draw_board method to Play state class

--- game_state/play.rb
+++ game_state/play.rb
@@ -69,12 +69,38 @@ class Play < GameState
     msg_hud
   end
 
-  def save
-
-  end
-
-  def quit
-
+  def draw_board matrix = self.state_opts "matrix"
+    top = "_"
+    side = "|"
+    # transpose the square arrays into one array row per pixel
+    matrix.each_with_index do |matrix_row, matrix_row_id|
+      row_arr = []
+      matrix_row.each_with_index do |square, square_id|
+        square.each_with_index do |square_row, square_row_id|
+          row_arr.push = [] unless row_arr[square_row_id]
+          row_arr[square_row_id].push [side, " "] if square_id == 0
+          row_arr[square_row_id].push [" ", side, " "] if square_id.between() > 0 && square_id < square.length - 1
+          row_arr[square_row_id].push square_row
+          row_arr[square_row_id].push [" ", side] if square_id == square.length - 1
+        end
+      end
+      # each horizontal border is composed of a "top" and "bottom" row
+      border_top = row_arr[0].map { |px| px == side ? " " : top }
+      border_bottom = row_arr[0].map { |px| px == side ? top : " " }
+      # add top border for each row
+      row_arr.unshift border_bottom
+      row_arr.unshift border_top
+      # for the last row, add a bottom row as well
+      row_arr.push border_top if matrix_row_id = matrix.length - 1
+      row_arr.push border_bottom if matrix_row_id = matrix.length - 1
+      # finally, add each display_row to the array
+      row_arr.each { | row | display_arr.push row.flatten }
+    end
+    # reduce array to a single string with /n separating rows of pixels
+    display_arr.reduce "" do |display, display_row|
+      display += display_row.join "" + "\n"
+      display
+    end
   end
 
 end
