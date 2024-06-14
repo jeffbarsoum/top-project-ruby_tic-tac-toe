494082a0e69ea4ac71d8e5a9647604e1ba9d4c1b
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-14 14:10:04 -0400

    Computer player wireframe

---
+++ class/computer.rb
@@ -0,0 +1,23 @@
+require "player"
+require "matrix"
+
+class Computer < Player
+
+  attr_reader :name, :player, :score
+
+
+  def initialize name, player_id
+      @name = name
+      @player = player_id
+      @score = 0
+  end
+
+  def add_score
+    self.score += 1
+  end
+
+  def to_s
+    puts self.player
+  end
+
+end
