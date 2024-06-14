cf7b02951397dd876a4718216e576fe982aff74a
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-14 06:00:06 -0400

    Remove extraneous code

--- module/display.rb
+++ module/display.rb
@@ -1,41 +1,10 @@
-require "tic_tac_toe"
-require "error"
 require "get_user_input"
-require "cmds"
 
 module Display
-  DISPLAY_CHOICES = {
-    title: { choices: [:start, :load, :save, :quit], vertical: true },
-    load: { choices: [:back, :quit], vertical: false },
-    game: { choices: [:save, :quit], vertical: true },
-    win: { choices: [:play_again, :save, :quit], vertical: true },
-  }
-
 
   include Error
   include GetUserInput
-  include DataCmds
 
-
-  attr_accessor :active
-
-
-  ### Deprecate these functions
-  def get_opts_array opts_hash
-    super opts_hash
-  end
-
-  def user_options command_arr
-    super command_arr
-  end
-
-  def board
-    super
-  end
-
-  def draw_board
-    self.board.draw_board
-  end
 
   def screen display:, state_hash:, state_cmds: [], screen_cmds: [], **opts
     vertical = opts[:vertical] || false
@@ -59,13 +28,6 @@ module Display
     end
   end
 
-  def display_hash state_cmds
-    state_cmds.reduce {} do |out_hash, (user_input, cmd_hash)|
-      out_hash[user_input.to_sym] = cmd_hash[:text]
-      out_hash
-    end
-  end
-
   def opts_display state_hash, vertical = false
     spacing = vertical ? "\n" : " | "
     return_string = self.print_spacing
@@ -85,131 +47,4 @@ module Display
     system "clear" || system "cls"
   end
 
-  def title choices
-
-    msg_title <<-STRING
-    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-                                        Tic Tac Toe!
-    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-
-    Please press a key to choose your option:
-
-    #{self.opts_display choices}
-
-    STRING
-    msg_title
-  end
-
-  def spacing
-    msg_spacing <<-STRING
-
-    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-
-    STRING
-    msg_spacing
-
-  end
-
-  def game players, stats
-    p1 = players[0]
-    p2 = players[1]
-    p1_score = stats[:score][p1.player.to_sym]
-    p2_score = stats[:score][p2.player.to_sym]
-    p1_turn = stats[:turn][p1.player.to_sym]
-    p2_score = stats[:turn][p2.player.to_sym]
-    rnd = stats[:round]
-
-    msg_hud <<-STRING
-
-    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-    Round ##{rnd}, Turn ##{p1_turn}
-    ------------------------------------------------------------------------------------
-    #{p1.name}'s Turn:
-    ------------------------------------------------------------------------------------
-    #{p1.name} (#{p1})                                            #{p2.name} (#{p2})
-    ----------------------                                        ----------------------
-    score: #{p1_score}                                            score: #{p2_score}
-    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-
-    STRING
-    msg_hud
-  end
-
-  def load data
-    print_save_list = ""
-    data.each_with_index do |save, i|
-      str_time = save.timestamp.strftime("%d/%m/%Y, %I:%M %p")
-      str_name = save.name
-      p1_score = "#{save.players[0].name}: #{save.players[0].score}"
-      p2_score = "#{save.players[1].name}: #{save.players[1].score}"
-
-      print_save_list += "${i + 1}) #{str_time} - #{str_name}:\n   #{p1_score}\n   #{p2_score}\n\n"
-    end
-
-    msg_save <<-STRING
-
-    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-    Choose your save, input the number shown to play your save:
-    ------------------------------------------------------------------------------------
-    #{print_save_list}
-    ------------------------------------------------------------------------------------
-    XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-
-    STRING
-
-    self.title
-    msg_hud
-  end
-
-  def win player, choices = [:play_again, :save, :quit]
-    player_win_msg = "*** #{player.name} Wins!! Ballin... ***"
-    player_win_msg += player_win_msg.length % 2 == 0 ? "" : " "
-    spacing_length = ((38 - player_win_msg.length) / 2).to_i
-    spacing = Array.new(spacing_length, " ").join
-    player_name = "#spacing"
-    #banner contains 2 rows and 38 characters each
-    #art borrowed from the below websites:
-    # https://www.angelfire.com/ca/mathcool/4july.html
-    # http://www.ae.metu.edu.tr/~cengiz/airplane-ascii.html
-
-    msg_win <<-STRING
-                                   ______________________________________
-                  , _.o..__  / \  /      *** BREAKING NEWS!!! ***        |
-                  +********=++-'-- #{spacing}#{player_win_msg}#{spacing} |
-                  ` o/            \______________________________________|
-
-
-
-                  .''.
-                  .''.      .        *''*    :_\/_:     .
-                :_\/_:   _\(/_  .:.*_\/_*   : /\ :  .'.:.'.
-              .''.: /\ :    /)\   ':'* /\ *  : '..'.  -=:o:=-
-              :_\/_:'.:::.  | ' *''*    * '.\'/.'_\(/_ '.':'.'
-              : /\ : :::::  =  *_\/_*     -= o =- /)\     '  *
-              '..'  ':::' === * /\ *     .'/.\'.  ' ._____
-                *        |   *..*         :       |.   |' .---"|
-                  *      |     _           .--'|  ||   | _|    |
-                  *      |  .-'|       __  |   |  |    ||      |
-                .-----.   |  |' |  ||  |  | |   |  |    ||      |
-              ___'       ' /"\ |  '-."".    '-'   '-.'    '`      |____
-              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                                  ~-~-~-~-~-~-~-~-~-~      /|
-                    )      ~-~-~-~-~-~-~-~      /|~       /_|\
-                  _-H-__  -~-~-~-~-~-~         /_|\    -~======-~
-              ~-\XXXXXXXXXX/~     ~-~-~-~     /__|_\ ~-~-~-~
-              ~-~-~-~-~-~    ~-~~-~-~-~-~    ========  ~-~-~-~
-                ~-~~-~-~-~-~-~-~-~-~-~-~-~ ~-~~-~-~-~-~
-                                  ~-~~-~-~-~-~
-
-XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-
-Thanks for playing! What would you like to do?
-
-XXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOOXXXOOO
-
-#{(self.opts_display choices).replace('Start', 'Play Again')}
-
-    STRING
-
-  end
+end
