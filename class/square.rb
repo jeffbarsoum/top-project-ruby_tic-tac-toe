25ff3efabd7af04432076bd496c0d6b15639ee31
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-14 22:33:50 -0400

    Remove error handling and extraeneous code

--- class/square.rb
+++ class/square.rb
@@ -1,88 +1,33 @@
 require "matrix"
 require "data"
-require "error"
 
-
-class Square < Matrix
-  include Data
-  include Error
+class Square
+  include DataSquares
 
   attr_reader :coordinates, :player, :square
+  attr_accessor :game_save
 
 
   def initialize coordinates
-    cls_name = "Square"
-    func_name = "initialize"
-    begin
-      @player = player.to_sym
-      @square = Data.nil_square coordinates[:text]
-      @coordinates = coordinates
-
-      raise BadPixelError unless self.is_correct_pixel
-      raise SquareSizeError unless self.is_correct_dim
-
-      self.square
-    rescue BadPixelError
-      msg_bad_pixel_error <<-STRING
-        each entry in the square array represents one pixel, and must be
-        exactly one character long
-      STRING
-      puts self.error cls_name, func_name, msg_bad_pixel_error
-    rescue SquareSizeError
-      msg_square_size_error <<-STRING
-        drawn square must be an array containing #{sq_rows} arrays,
-        each containing #{sq_cols} string entries
-      STRING
-      puts self.error cls_name, func_name, msg_square_size_error
-    end
+    @player = nil
+    @square = self.nil_square coordinates[:text]
+    @coordinates = coordinates
+    @game_save = {}
   end
 
-  def is_correct_pixel
-    self.square.reduce true do |is_row_one_pixel, pixel_row|
-      pixel_row.reduce is_row_one_pixel do |is_one_pixel, pixel|
-        break false unless is_one_pixel
-        is_one_pixel &&  pixel.length == 1
-      end
-    end
-  end
-
-  def is_correct_dim
-    self.square.reduce true do |is_correct_rows, pixel_row|
-      break false unless square_array.length == self.sq_rows
-      is_correct_rows && pixel_row.length == self.sq_cols
-    end
-  end
-
-  def is_assigned
+  def is_assigned?
     self.player ? true : false
   end
 
   def assign_player player
-    err_prefix = "Square.assign_player ERROR"
-    begin
-      raise PlayerAssignError if self.is_assigned
-      @player = player
-      @square = Data.square player
-    rescue PlayerAssignError
-      msg_player_assigned_error <<-STRING
-        square #{coordinates} is already assigned to player #{player}
-      STRING
-      puts self.error err_prefix, msg_player_assigned_error
-    end
+    return false if self.is_assigned
+    @player = player
+    @square = self.square player
   end
 
   def assign_win win_type
-    err_prefix = "Square.assign_win ERROR"
-    begin
-      raise WinAssignError if @player.nil?
-      @square = Data.square self.player, win_type.to_sym
-    rescue WinAssignError
-      msg_win_assigned_error <<-STRING
-        square must be assigned a player before it can be assigned
-        a win!
-      STRING
-      puts self.error err_prefix, msg_win_assigned_error
-    end
+    @square = self.square self.player, win_type
+  end
 
   def to_s
     puts self.player
