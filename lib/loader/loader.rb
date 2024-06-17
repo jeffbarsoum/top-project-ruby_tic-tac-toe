82944ca4f3f97a5b8bb3bc23b55061998ff8d39e
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-16 21:05:50 -0400

    Add get_loader method to choose concrete loader based on user input

--- lib/loader/loader.rb
+++ lib/loader/loader.rb
@@ -1,4 +1,15 @@
+require "lib/module/error"
+require "lib/module/variablize"
+
 class Loader
+    include Error
+    include Variablize
+
+    def get_loader game
+        raise NotImplementedError, self.error "Script not found" unless require_relative game
+        cls_name = self.script_name, "Loader::"
+        Object.const_get cls_name
+    end
 
     def load_players
         raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
