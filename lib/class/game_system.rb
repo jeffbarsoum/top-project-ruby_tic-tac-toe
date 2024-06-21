e92002178b7221610c627e871e0d9a03cc782140
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-06-21 10:17:32 -0400

    Remove load_game method from abstract GameSystem (Loader class handles that separatly, and is called by client to get Concrete GameSystem)

--- lib/class/game_system.rb
+++ lib/class/game_system.rb
@@ -1,20 +1,4 @@
-require "lib/module/error"
-require "lib/module/variablize"
-
 class GameSystem
-    include Error
-    include Variablize
-
-    attr_reader :load_script, :game_dir
-
-
-    def get_loader game
-        raise NotImplementedError, self.error "Script not found" unless require_relative game
-        @load_script = game
-        @game_dir = "lib/game/#{game}"
-        cls_name = self.script_name, "Loader::"
-        Object.const_get cls_name
-    end
 
     def load_cmds
         raise NotImplementedError, self.error "Method not implemented, famo..."
